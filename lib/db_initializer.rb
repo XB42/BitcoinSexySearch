b362f4b94d8614f52ea2354db3cba84b20cb3425
Bux Khurana <buxkhurana98@gmail.com>
2023-02-09 13:05:27 +0400

    Added front end

--- lib/db_initializer.rb
+++ lib/db_initializer.rb
@@ -1,4 +1,4 @@
-require_relative 'apicaller.rb'
+equire_relative 'apicaller.rb'
 require_relative 'view.rb'
 require 'csv'
 class DBInitialiser
@@ -6,10 +6,10 @@ class DBInitialiser
     filepath = "../db/database.csv"
     i = APICaller.get_max_height()
     CSV.open(filepath, "wb") do |csv|
-        csv << ["height", "tx_count", "Reward", "Fee", "Difficulty", "Reward", "Size", "Time"]
+        csv << ["height", "tx_count", "Reward", "Fee", "Difficulty", "Size", "Time"]
         (1..i).each do |block|
           blockhash = APICaller.callAPI(block.to_s)
-          csv << [blockhash[0]["height"], blockhash[0]["tx_count"], blockhash[0]["reward"], blockhash[0]["fee"], blockhash[0]["difficulty"], blockhash[0]["reward"], blockhash[0]["size"], blockhash[0]["time"]]
+          csv << [blockhash[0]["height"], blockhash[0]["tx_count"], blockhash[0]["reward"], blockhash[0]["fee"], blockhash[0]["difficulty"], blockhash[0]["size"], blockhash[0]["time"]]
         end
     end
   end
