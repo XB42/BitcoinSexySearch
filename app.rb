710e41e7a8377774ef096dff241a9725c75cf1c5
Bux Khurana <buxkhurana98@gmail.com>
2023-02-15 12:12:27 +0400

    fixed file structure, created routing properly

--- app.rb
+++ app.rb
@@ -2,16 +2,7 @@ require 'sinatra'
 require_relative 'web/layoutcreator.rb'
 require_relative 'web/entrycreator.rb'
 class View < Sinatra::Base
-  enable :sessions
   get '/' do
-    session[:entries] = 1 if session[:entries] == nil
-    LayoutCreator.layout do
-      EntryCreator.create_entry * session[:entries]
+    erb :index
     end
-  end
-
-  get '/add' do
-    session[:entries] += 1
-    redirect '/'
-  end
 end
