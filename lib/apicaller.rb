05de35fbef9d0ddf0b89f5b27ed4a577e6f6aa0f
Bux Khurana <buxkhurana98@gmail.com>
2023-02-17 06:45:31 +0400

    Optimised db initialiser to be 100X faster

--- lib/apicaller.rb
+++ lib/apicaller.rb
@@ -3,7 +3,7 @@ require 'json'
 class APICaller
 
   def self.callAPI(block_number)
-    url = "https://api-r.bitcoinchain.com/v1/block/" + block_number + "/withTx"
+    url = "https://api-r.bitcoinchain.com/v1/block/" + block_number #+ "/withTx"
     doc = URI.open(url).read
     hash = JSON.parse(doc)
     p hash
